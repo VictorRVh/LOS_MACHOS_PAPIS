6b3af7fe7652e6ffbc07d6832408dba22e7a0c50
victor raul <victorraulvaldez54@gmail.com>
2025-09-30 16:21:57 -0500

    fromInput mayusculas

--- resources/js/src/components/ui/FormInput.vue
+++ resources/js/src/components/ui/FormInput.vue
@@ -1,121 +1,125 @@
+```vue
 <script setup>
-import { computed, ref } from 'vue';
-import FormLabelError from './FormLabelError.vue';
-
-import { v4 } from 'uuid';
+import { computed, ref } from 'vue'
+import FormLabelError from './FormLabelError.vue'
+import { v4 } from 'uuid'
 
 const props = defineProps({
-    modelValue: {
-        required: false,
-    },
-    type: {
-        type: String,
-        default: () => 'text',
-    },
-    label: {
-        type: String,
-    },
-    labelClass: {
-        type: String,
-        default: () => '',
-    },
-    placeholder: {
-        type: String,
-    },
-    disabled: {
-        type: Boolean,
-        default: () => false,
-    },
-    inputClass: {
-        type: String,
-        default: () => '',
-    },
-    error: {
-        type: [String, null],
-        default: () => null,
-    },
-    errorClass: {
-        type: String,
-        default: () => '',
-    },
-    focus: {
-        type: Boolean,
-        default: () => false,
-    },
-    required: {
-        type: Boolean,
-        default: () => false,
-    },
-    step: {
-        type: [String, Number, null],
-        default: () => null,
-    },
-    vModelOnBlur: {
-        type: Boolean,
-        default: () => false,
-    },
-        uppercase: {
-        type: Boolean,
-        default: false,
-    },
-});
+  modelValue: {
+    required: false,
+  },
+  type: {
+    type: String,
+    default: () => 'text',
+  },
+  label: {
+    type: String,
+  },
+  labelClass: {
+    type: String,
+    default: () => '',
+  },
+  placeholder: {
+    type: String,
+  },
+  disabled: {
+    type: Boolean,
+    default: () => false,
+  },
+  inputClass: {
+    type: String,
+    default: () => '',
+  },
+  error: {
+    type: [String, null],
+    default: () => null,
+  },
+  errorClass: {
+    type: String,
+    default: () => '',
+  },
+  focus: {
+    type: Boolean,
+    default: () => false,
+  },
+  required: {
+    type: Boolean,
+    default: () => false,
+  },
+  step: {
+    type: [String, Number, null],
+    default: () => null,
+  },
+  vModelOnBlur: {
+    type: Boolean,
+    default: () => false,
+  },
+  uppercase: {
+    type: Boolean,
+    default: false,
+  },
+})
 
-const emit = defineEmits(['update:modelValue', 'focus', 'blur']);
+const emit = defineEmits(['update:modelValue', 'focus', 'blur'])
 
 const value = computed({
-    get: () => props.modelValue,
-    set: (value) => {
-        if (!props.vModelOnBlur) emit('update:modelValue', value);
-    },
-});
-const id = ref(`input-${v4()}`);
+  get: () => props.modelValue,
+  set: (val) => {
+    const newVal = props.uppercase ? val?.toUpperCase() : val
+    if (!props.vModelOnBlur) emit('update:modelValue', newVal)
+  },
+})
+
+const id = ref(`input-${v4()}`)
 
 const onKeydown = (event) => {
-    if (props.type !== 'number') return;
-    disableKeys(event, ['e', 'E']);
-};
+  if (props.type !== 'number') return
+  disableKeys(event, ['e', 'E'])
+}
 
 const onFocus = (event) => {
-    emit('focus', event?.target?.value ? event.target.value : null, event);
-};
+  emit('focus', event?.target?.value ? event.target.value : null, event)
+}
 
 const onBlur = (event) => {
-    const val = event?.target?.value ? event.target.value : null;
-    emit('blur', val, event);
-    if (props.vModelOnBlur) emit('update:modelValue', val);
-};
+  const val = event?.target?.value ? event.target.value : null
+  const newVal = props.uppercase ? val?.toUpperCase() : val
+  emit('blur', newVal, event)
+  if (props.vModelOnBlur) emit('update:modelValue', newVal)
+}
 
 const disableKeys = (event, keys = ['e', 'E', '+', '-']) => {
-    if (!keys) return;
-    keys.includes(event.key) && event.preventDefault();
-};
+  if (!keys) return
+  keys.includes(event.key) && event.preventDefault()
+}
 </script>
 
 <template>
-    <FormLabelError
-        :label="label"
-        :label-class="labelClass"
-        :error="error"
-        error-class="errorClass"
-        :required="required"
-    >
-        <input
-            v-model="value"
-            v-focus.select="focus"
-            :id="id"
-            :type="type"
-            :step="step ? step : type === 'number' ? 'any' : null"
-            class="bg-gray-50 border border-cetpro sm:text-sm rounded-sm block w-full sm:p-1.5 xl:p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 focus:ring-2 focus:ring-cetpro-light focus:border-cetpro-light dark:focus:ring-cetpro-dark dark:focus:border-cetpro-dark outline-none"
-            :placeholder="placeholder ? placeholder : ''"
-            :class="[
-                disabled ? 'cursor-not-allowed' : '',
-                inputClass ? inputClass : '',
-                uppercase ? 'uppercase' : '',
-            ]"
-            :disabled="disabled"
-            @keydown="onKeydown"
-            @focus="onFocus"
-            @blur="onBlur"
-        />
-    </FormLabelError>
+  <FormLabelError
+    :label="label"
+    :label-class="labelClass"
+    :error="error"
+    error-class="errorClass"
+    :required="required"
+  >
+    <input
+      v-model="value"
+      v-focus.select="focus"
+      :id="id"
+      :type="type"
+      :step="step ? step : type === 'number' ? 'any' : null"
+      class="bg-gray-50 border border-cetpro sm:text-sm rounded-sm block w-full sm:p-1.5 xl:p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 focus:ring-2 focus:ring-cetpro-light focus:border-cetpro-light dark:focus:ring-cetpro-dark dark:focus:border-cetpro-dark outline-none"
+      :placeholder="placeholder ? placeholder : ''"
+      :class="[
+        disabled ? 'cursor-not-allowed' : '',
+        inputClass ? inputClass : '',
+        uppercase ? 'uppercase' : '',
+      ]"
+      :disabled="disabled"
+      @keydown="onKeydown"
+      @focus="onFocus"
+      @blur="onBlur"
+    />
+  </FormLabelError>
 </template>
+```

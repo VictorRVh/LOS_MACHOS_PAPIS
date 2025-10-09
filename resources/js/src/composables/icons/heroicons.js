854b2e5b39fca6e79767a21a5b6f17de7596ecab
William <kloudw1ld@gmail.com>
2025-10-09 15:22:16 -0500

    cambiosDocs

--- resources/js/src/composables/icons/heroicons.js
+++ resources/js/src/composables/icons/heroicons.js
@@ -1,68 +1,66 @@
 // src/plugins/heroicons.js
-import { NewspaperIcon, PaperClipIcon } from '@heroicons/vue/24/outline';
 import { defineAsyncComponent } from 'vue';
 
-// Importa los íconos en estilo outline
 const icons = {
+  // Navigation & General UI
   HomeIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/HomeIcon')),
-  BeakerIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/BeakerIcon')),
-  UserIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/UserIcon')),
-  AcademicCapIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/AcademicCapIcon')),
-  UsersIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/UsersIcon')),
-  UserGroupIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/UserGroupIcon')),
-  BookOpenIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/BookOpenIcon')),
-  BuildingOfficeIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/BuildingOfficeIcon')),
-  PresentationChartLineIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/PresentationChartLineIcon')),
-  ChartBarIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/ChartBarIcon')),
-  FolderIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/FolderIcon')),
-  BookmarkIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/BookmarkIcon')),
-  BookmarkSquareIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/BookmarkSquareIcon')),
-  EyeIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/EyeIcon')),
+  Bars3Icon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/Bars3Icon')),
+  MagnifyingGlassIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/MagnifyingGlassIcon')),
+  AdjustmentsVerticalIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/AdjustmentsVerticalIcon')),
+  EllipsisVerticalIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/EllipsisVerticalIcon')),
+  ChevronDownIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/ChevronDownIcon')),
+  ArrowLeftIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/ArrowLeftIcon')),
+  ArrowUpRightIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/ArrowUpRightIcon')),
+  InformationCircleIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/InformationCircleIcon')),
+
+  // Actions
+  PencilIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/PencilIcon')),
   PencilSquareIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/PencilSquareIcon')),
   TrashIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/TrashIcon')),
-  CalendarIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/CalendarIcon')),
-  ClipboardDocumentListIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/ClipboardDocumentListIcon')),
-  PaperClipIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/PaperClipIcon')),
-  NewspaperIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/NewspaperIcon')), 
-  DocumentIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/DocumentIcon')), 
-  DocumentArrowDownIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/DocumentArrowDownIcon')), 
-  // DownloadIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/DownloadIcon')),
-  BookOpenIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/BookOpenIcon')),
-  Bars3Icon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/Bars3Icon')),
-  // Agrega más íconos aquí según sea necesario
+  EyeIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/EyeIcon')),
+  ArrowUpTrayIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/ArrowUpTrayIcon')),
+  ArrowDownTrayIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/ArrowDownTrayIcon')),
+  ArrowPathIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/ArrowPathIcon')),
+  SquaresPlusIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/SquaresPlusIcon')),
+  UserPlusIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/UserPlusIcon')),
 
-  ShieldCheckIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/ShieldCheckIcon')), 
-  KeyIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/KeyIcon')),
-  GlobeEuropeAfricaIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/GlobeEuropeAfricaIcon')),
-  UserCircleIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/UserCircleIcon')),
-  ArrowLeftIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/ArrowLeftIcon')),
-  MagnifyingGlassIcon:  defineAsyncComponent( () => import('@heroicons/vue/24/outline/MagnifyingGlassIcon')),
-  AdjustmentsVerticalIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/AdjustmentsVerticalIcon')),
-  PencilIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/PencilIcon')),
-  ArrowDownTrayIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/ArrowDownTrayIcon')),
-  CalendarDateRangeIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/CalendarDateRangeIcon')),
-  PresentationChartLineIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/PresentationChartLineIcon')),
-  AcademicCapIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/AcademicCapIcon')),
-  SquaresPlusIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/SquaresPlusIcon')),
-  RectangleGroupIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/RectangleGroupIcon')),
-  NewspaperIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/NewspaperIcon')),
-  ClockIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/ClockIcon')),
-  CheckCircleIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/CheckCircleIcon')),
-  ArrowUpTrayIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/ArrowUpTrayIcon')),
-  ChevronDownIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/ChevronDownIcon')),
-  ArchiveBoxIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/ArchiveBoxIcon')),
-  ArrowPathIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/ArrowPathIcon')),
-  EyeIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/EyeIcon')),
-  DocumentTextIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/DocumentTextIcon')),
-  ArrowUpRightIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/ArrowUpRightIcon')),
-  ClipboardDocumentCheckIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/ClipboardDocumentCheckIcon')),
-  UserPlusIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/UserPlusIcon')),
-  InformationCircleIcon: defineAsyncComponent( () => import('@heroicons/vue/24/outline/InformationCircleIcon')),
-  ///___________________________________
+  // Objects & Documents
+  FolderIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/FolderIcon')),
+  DocumentIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/DocumentIcon')),
+  DocumentTextIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/DocumentTextIcon')),
+  DocumentArrowDownIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/DocumentArrowDownIcon')),
+  NewspaperIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/NewspaperIcon')),
+  PaperClipIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/PaperClipIcon')),
+  ArchiveBoxIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/ArchiveBoxIcon')),
+  BookmarkIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/BookmarkIcon')),
+  BookmarkSquareIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/BookmarkSquareIcon')),
+  ClipboardDocumentListIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/ClipboardDocumentListIcon')),
+  ClipboardDocumentCheckIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/ClipboardDocumentCheckIcon')),
 
+  // Users & Roles
+  UserIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/UserIcon')),
+  UsersIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/UsersIcon')),
+  UserGroupIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/UserGroupIcon')),
+  UserCircleIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/UserCircleIcon')),
+  ShieldCheckIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/ShieldCheckIcon')),
+  KeyIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/KeyIcon')),
+
+  // Academic & Data
+  AcademicCapIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/AcademicCapIcon')),
+  BookOpenIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/BookOpenIcon')),
+  BuildingOfficeIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/BuildingOfficeIcon')),
+  RectangleGroupIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/RectangleGroupIcon')),
+  PresentationChartLineIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/PresentationChartLineIcon')),
+  ChartBarIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/ChartBarIcon')),
+  BeakerIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/BeakerIcon')),
+  GlobeEuropeAfricaIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/GlobeEuropeAfricaIcon')),
+
+  // Time & Status
+  CalendarIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/CalendarIcon')),
+  ClockIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/ClockIcon')),
+  CheckCircleIcon: defineAsyncComponent(() => import('@heroicons/vue/24/outline/CheckCircleIcon')),
 };
 
-// Registra los íconos como componentes globales
 export function registerHeroIcons(app) {
   for (const [name, icon] of Object.entries(icons)) {
     app.component(name, icon);
--- resources/js/src/pages/Grupo/GrupoDocumentos.vue
+++ resources/js/src/pages/Grupo/GrupoDocumentos.vue
@@ -1,17 +1,12 @@
 <script setup>
 import { ref, onMounted } from 'vue';
-import { EyeIcon, ArrowUpTrayIcon } from '@heroicons/vue/24/outline';
+import { useRouter } from 'vue-router';
+import { FolderIcon, ArrowUpRightIcon } from '@heroicons/vue/24/outline'; // Asegúrate de importar ArrowUpRightIcon
 import useTableData from '../../composables/tabla/useTableData';
 import useSlider from '../../composables/useSlider';
 
 import SearchBar from '../../components/head_table/headSearch.vue';
 import CreateButton from '../../components/ui/CreateButton.vue';
-import Table from '../../components/table/Table.vue';
-import THead from '../../components/table/THead.vue';
-import TBody from '../../components/table/TBody.vue';
-import Tr from '../../components/table/Tr.vue';
-import Th from '../../components/table/Th.vue';
-import Td from '../../components/table/Td.vue';
 import GrupoDocumentoSlider from '../../components/page/Grupo/GrupoDocumentoSlider.vue';
 
 const props = defineProps({
@@ -21,20 +16,17 @@ const props = defineProps({
   },
 });
 
+const router = useRouter();
 const documentos = ref([]);
 const { slider, sliderData, showSlider, hideSlider } = useSlider();
 
 const {
-  query,
-  pagina,
-  itemsPorPagina,
   paginados: documentosPaginados,
-  totalPaginas,
   ordenados: documentosOrdenados,
   filtrar: filtrarDocumentos
 } = useTableData(documentos, {
   defaultOrderBy: "titulo",
-  searchFields: ["titulo", "descripcion", "observaciones"]
+  searchFields: ["titulo"]
 });
 
 const refreshDocumentos = () => {
@@ -43,62 +35,61 @@ const refreshDocumentos = () => {
 
 onMounted(() => {
   documentos.value = [
-    { id: 1, fecha_inicio: '10/03/2025', titulo: 'Sílabo', descripcion: '-', observaciones: 'falta mas datos' },
-    { id: 2, fecha_inicio: '10/03/2025', titulo: 'Sesiones', descripcion: '-', observaciones: 'falta mas datos' },
-    { id: 3, fecha_inicio: '10/03/2025', titulo: 'Capacidades terminales', descripcion: '-', observaciones: 'falta mas datos' },
-    { id: 4, fecha_inicio: '10/03/2025', titulo: 'Documentos', descripcion: '-', observaciones: 'falta mas datos' },
+    { id: 1, titulo: 'Sílabo', descripcion: 'Plan de estudios detallado.' },
+    { id: 2, titulo: 'Sesiones de Clase', descripcion: 'Material y planificación de cada sesión.' },
+    { id: 3, titulo: 'Capacidades Terminales', descripcion: 'Objetivos y competencias del módulo.' },
+    { id: 4, titulo: 'Registro de Asistencia', descripcion: 'Control de asistencia de alumnos.' },
+    { id: 5, titulo: 'Registro de Evaluaciones', descripcion: 'Calificaciones y notas de los alumnos.' },
   ];
 });
 
-const verDocumento = (doc) => {
-  console.log('Viendo documento:', doc.id);
+const abrirCarpeta = (doc) => {
+  console.log('Navegando a la carpeta del documento:', doc.id);
+  // Descomenta la siguiente línea y ajusta el 'name' de la ruta cuando la tengas definida
+  // router.push({ name: 'grupo.documentos.detalle', params: { id: props.id, docId: doc.id } });
 };
 </script>
 
 <template>
-  <div class="space-y-4">
+  <div class="space-y-6">
     <div class="flex justify-between items-center">
       <h3 class="text-xl font-semibold text-gray-600 dark:text-gray-300">
         Lista de documentos
       </h3>
-      <CreateButton @click="showSlider(true)">
-        Agregar Nuevo
-      </CreateButton>
+      <div class="flex items-center gap-4">
+        <SearchBar :totalResultados="documentosOrdenados.length" @search="filtrarDocumentos" />
+        <CreateButton @click="showSlider(true)">
+            Agregar Nuevo
+        </CreateButton>
+      </div>
     </div>
     
-    <div class="flex justify-end">
-        <SearchBar :totalResultados="documentosOrdenados.length" @search="filtrarDocumentos" />
+    <div v-if="documentosPaginados.length > 0" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      <div v-for="doc in documentosPaginados" :key="doc.id" 
+           @click="abrirCarpeta(doc)"
+           class="group relative bg-white dark:bg-gray-800 rounded-lg shadow-md p-6 border border-gray-200 dark:border-gray-700 cursor-pointer transition-all duration-300 ease-in-out hover:shadow-xl hover:-translate-y-1 hover:border-cetpro/50">
+        
+        <div class="flex justify-between items-start">
+            <FolderIcon class="h-12 w-12 text-cetpro dark:text-cetpro-light transition-colors duration-300" />
+            <ArrowUpRightIcon class="h-6 w-6 text-gray-400 dark:text-gray-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+        </div>
+
+        <div class="mt-4">
+            <h4 class="text-lg font-bold text-gray-800 dark:text-white transition-colors duration-300 group-hover:text-cetpro-dark dark:group-hover:text-cetpro-light">
+                {{ doc.titulo }}
+            </h4>
+            <p class="mt-1 text-sm text-gray-500 dark:text-gray-400 h-10">
+                {{ doc.descripcion }}
+            </p>
+        </div>
+      </div>
     </div>
 
-    <Table :paginacion="true" :current-page="pagina" :total-pages="totalPaginas" @changePage="pagina = $event">
-      <THead>
-        <Th>N°</Th>
-        <Th>Fecha Inicio</Th>
-        <Th>Título</Th>
-        <Th>Descripción</Th>
-        <Th>Observaciones</Th>
-        <Th class="text-center">Opciones</Th>
-      </THead>
-      <TBody>
-        <Tr v-for="(doc, index) in documentosPaginados" :key="doc.id">
-          <Td>{{ (pagina - 1) * itemsPorPagina + index + 1 }}</Td>
-          <Td>{{ doc.fecha_inicio }}</Td>
-          <Td class="font-medium">{{ doc.titulo }}</Td>
-          <Td>{{ doc.descripcion }}</Td>
-          <Td>{{ doc.observaciones }}</Td>
-          <Td>
-            <div class="flex items-center justify-center space-x-2">
-              <button @click="showSlider(true, doc)" class="p-1 text-gray-500 hover:text-blue-600 transition-colors">
-                <ArrowUpTrayIcon class="h-5 w-5" />
-              </button>
-              <button @click="verDocumento(doc)" class="p-1 text-gray-500 hover:text-blue-600 transition-colors">
-                <EyeIcon class="h-5 w-5" />
-              </button>
-            </div>
-          </Td>
-        </Tr>
-      </TBody>
-    </Table>
+    <div v-else class="text-center py-16 bg-white dark:bg-gray-800 rounded-lg shadow-md">
+        <FolderIcon class="mx-auto h-16 w-16 text-gray-300 dark:text-gray-600" />
+        <h3 class="mt-2 text-lg font-semibold text-gray-800 dark:text-gray-200">No se encontraron documentos</h3>
+        <p class="mt-1 text-sm text-gray-500">Intenta con otra búsqueda o agrega un nuevo tipo de documento.</p>
+    </div>
   </div>
   
   <GrupoDocumentoSlider 

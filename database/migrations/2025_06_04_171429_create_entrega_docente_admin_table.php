21bd70f6ef9fddd228d048c9b74696f9ba36c395
victor raul <victorraulvaldez54@gmail.com>
2025-10-27 10:12:59 -0500

    errores

--- database/migrations/2025_06_04_171429_create_entrega_docente_admin_table.php
+++ database/migrations/2025_06_04_171429_create_entrega_docente_admin_table.php
@@ -14,17 +14,9 @@ return new class extends Migration
         Schema::create('entrega_docente_admin', function (Blueprint $table) {
             $table->uuid('id')->primary();
 
-<<<<<<< HEAD
             $table->string('tipo_entrega', 255);
             $table->dateTime('fecha_inicio')->nullable();
             $table->dateTime('fecha_fin')->nullable();
-=======
-            $table->tinyInteger('tipo_entrega');
-            $table->string('nombre_entrega', 255);
-            $table->dateTime('fecha_inicio')->nullable();
-            $table->dateTime('fecha_fin')->nullable();
-
->>>>>>> f2878b34cbce9301735378d1394f4c2bf1f1243e
             $table->tinyInteger('status')->default(0);
 
             $table->uuid('id_periodo');
@@ -32,11 +24,6 @@ return new class extends Migration
 
             $table->boolean('mostrar')->default(0);
 
-<<<<<<< HEAD
-=======
-            $table->boolean('sub_grupos')->default(0);
-
->>>>>>> f2878b34cbce9301735378d1394f4c2bf1f1243e
             $table->boolean('is_deleted')->default(0);
 
             $table->timestamps();
